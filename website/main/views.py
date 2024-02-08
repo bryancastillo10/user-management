6eda3b6cf40cdabca5fee62d99db3c65076e38f8
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-02-08 10:22:33 +0800

    sign up template

--- website/main/views.py
+++ website/main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
+
+ def sign_up(request):
+    if request.method="POST":
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/sign_up.html',{"form":form})
+
+       
